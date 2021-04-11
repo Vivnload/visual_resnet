2d1a3be3361e4e86092c531c8cd2999eaac71101
crazy_monkey <1013977246@qq.com>
2021-04-11 15:46:30 +0800

    sub_V0.01

--- sequence_modeling.py
+++ sequence_modeling.py
@@ -21,23 +21,27 @@ class Sub_BidirectionalLSTM(nn.Module):
 
     def __init__(self, input_size, hidden_size, output_size):
         super(Sub_BidirectionalLSTM, self).__init__()
-        self.cnn=nn.Conv1d(in_channels=input_size,out_channels=512,kernel_size=3)
-        self.max_pool=nn.MaxPool1d(kernel_size=6,stride=2)
-        self.relu=nn.ReLU(inplace=True)
         self.rnn = nn.LSTM(input_size, hidden_size, bidirectional=True, batch_first=True)
         self.linear = nn.Linear(hidden_size * 2, output_size)
+        self.maxpool = nn.MaxPool2d((1, 25), stride=(1, 25))
 
     def forward(self, input):
         """
         input : visual feature [batch_size x T x input_size]
         output : contextual feature [batch_size x T x output_size]
         """
-        input=input.permute(0,2,1)
-        input=self.cnn(input)
-        input=self.relu(input)
-        input=self.max_pool(input)
-        input = input.permute(0, 2, 1)
         self.rnn.flatten_parameters()
-        recurrent, _ = self.rnn(input)  # batch_size x T x input_size -> batch_size x T x (2*hidden_size)
-        output = self.linear(recurrent)  # batch_size x T x output_size
+        recurrents=[self.rnn(input[:,:i,:]) for i in range(1,26)]
+        recurrents_inv=[self.rnn(input[:,j:,:]) for j in range(1,26)]
+
+        pre_recurrents=[recurrent for recurrent, _ in recurrents]
+        suf_recurrents=[recurrent for recurrent, _ in recurrents_inv]
+
+        combines=[torch.cat((x,y),dim=1) for x,y in zip(pre_recurrents,suf_recurrents)]
+        combine=torch.cat(combines,dim=2)
+        new_input=self.maxpool(combine)
+
+        recurrent, _ = self.rnn(new_input)  # batch_size x T x input_size -> batch_size x T x (2*hidden_size)
+        output=self.linear(recurrent)
+
         return output
