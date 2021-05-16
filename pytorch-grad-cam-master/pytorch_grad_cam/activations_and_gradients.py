f1a620cd8071ce407e22e22639d83462a6e7b019
crazy_monkey <1013977246@qq.com>
2021-05-16 20:52:05 +0800

    test

---
+++ pytorch-grad-cam-master/pytorch_grad_cam/activations_and_gradients.py
@@ -0,0 +1,34 @@
+import torch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+class ActivationsAndGradients:
+    """ Class for extracting activations and
+    registering gradients from targetted intermediate layers """
+
+    def __init__(self, model, target_layer, reshape_transform):
+        self.model = model
+        self.gradients = []
+        self.activations = []
+        self.reshape_transform = reshape_transform
+
+        target_layer.register_forward_hook(self.save_activation)
+        target_layer.register_backward_hook(self.save_gradient)
+
+    def save_activation(self, module, input, output):
+        activation = output
+        if self.reshape_transform is not None:
+            activation = self.reshape_transform(activation)
+        self.activations.append(activation.cpu())
+
+    def save_gradient(self, module, grad_input, grad_output):
+        # Gradients are computed in reverse order
+        grad = grad_output[0]
+        if self.reshape_transform is not None:
+            grad = self.reshape_transform(grad)
+        self.gradients = [grad.cpu()] + self.gradients
+
+    def __call__(self, x):
+        self.gradients = []
+        self.activations = []
+        batch_size = x.size(0)
+        text_for_pred = torch.LongTensor(batch_size, 25 + 1).fill_(0).to(device)
+        return self.model(x,text_for_pred)
